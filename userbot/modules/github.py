94246cb06857ab8b945ca272685f945cd8614202
aNoobBot <63371589+aNoobBot@users.noreply.github.com>
2020-06-26 21:41:55 +0530

    Revert "SQUASHED: fixes from deepsource bot"
    This reverts commit 7ad8812a6031e501f1a1ad38661826205e6871cc.

--- userbot/modules/github.py
+++ userbot/modules/github.py
@@ -116,7 +116,7 @@ async def git_commit(file_name, mone):
             return await mone.edit("`File Already Exists`")
             create_file = False
     file_name = "userbot/modules/" + file_name
-    if create_file is True:
+    if create_file == True:
         file_name = file_name.replace("./userbot/temp/", "")
         print(file_name)
         try:
